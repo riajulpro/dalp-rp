9c36bb84eaa103f97a863600da7d2b1a5b17167c
RiaJul Pro <riajulpro@outlook.com>
2024-03-13 10:38:49 +0600

    Fixing the footer pixel perfect

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,43 +5,45 @@ const Footer = () => {
   return (
     <footer className="lg-width">
       <div className="footer-container">
-        <div className="footer content-box">
-          <div className="center">
-            <h4 className="section-title">Digital Agency</h4>
+        <article className="footer content-box">
+          <section className="center gap-right-normal">
+            <h4 className="section-title mb-40">Digital Agency</h4>
             <p className="footer-description">
               Building digital products, brands & experience
             </p>
-          </div>
-          <div>
-            <h4 className="section-title">Resources</h4>
+          </section>
+          <section>
+            <h4 className="section-title mb-32 gap-right-normal">Resources</h4>
             <div className="footer-links">
               <Link href="/">Guides</Link>
-              <Link href="/">Blogs</Link>
+              <Link href="/">Blog</Link>
               <Link href="/">Customer Stories</Link>
               <Link href="/">Glossary</Link>
             </div>
-          </div>
-          <div>
-            <h4 className="section-title">Company</h4>
+          </section>
+          <section>
+            <h4 className="section-title mb-32 gap-right-social">Company</h4>
             <div className="footer-links">
               <Link href="/">About Us</Link>
               <Link href="/">Career</Link>
               <Link href="/">Partners</Link>
               <Link href="/">Contact Us</Link>
             </div>
-          </div>
-          <div>
-            <h4 className="section-title">Social Media</h4>
+          </section>
+          <section>
+            <h4 className="section-title mb-32">Social Media</h4>
             <div className="footer-links">
               <Link href="/">Linkedin</Link>
               <Link href="/">Facebook</Link>
               <Link href="/">Instagram</Link>
               <Link href="/">Twitter</Link>
             </div>
-          </div>
-        </div>
+          </section>
+        </article>
+        <article className="copyright">
+          © Matheus. Todos os direitos reservados
+        </article>
       </div>
-      <div className="copyright">© Matheus. Todos os direitos reservados</div>
     </footer>
   );
 };
